0bc9c4a423c740afd0456a64c4d2990f597a00a7
donut <edwardtdo@gmail.com>
2022-09-03 13:01:05 +1000

    added register link to navbar

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,8 @@ function App() {
         Nav coming soon |
         <Link to='/'>Home</Link> {" "} |
         <Link to='/admin'>Admin</Link> {" "} |
-        <Link to='/learn'>Learn</Link>
+        <Link to='/learn'>Learn</Link> {" "} |
+        <Link to='/register'>Register</Link>
       </div>
       <Outlet/>
     </div>
